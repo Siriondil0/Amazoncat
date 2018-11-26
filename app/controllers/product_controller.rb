30a3ce29d43eaaa3866a00d270e3df89c56487cd
Samuel <sassani134@gmail.com>
2018-11-26 14:34:57 +0100

    def show ok

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,4 +1,12 @@
 class ProductController < ApplicationController
+	before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index 
+    @items = Item.order(1)
+    puts @items
   end
+def show
+  	  @item= Item.find(params[:id])
+  end
+​
+  
 end
