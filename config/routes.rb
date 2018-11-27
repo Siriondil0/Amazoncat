831cc7e658409795a576e57ca14a73aa2b20bac0
Samuel <sassani134@gmail.com>
2018-11-27 15:41:59 +0100

    je ne peux pas voir lerreur

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get '/test', to: 'test#test'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :product
+  resources :charges
 end
