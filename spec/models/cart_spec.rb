ddd9a65e0ed6a6179fb8c7e7ac44695c07568aa8
guillaume <elros.du.13@gmail.com>
2018-11-28 10:17:20 +0100

    test cart

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  
+  it 'can be created' do
+    user = User.create(
+    username: 'testtest',
+    email: 'test@example.com', 
+    password: 'password123',
+    password_confirmation: 'password123'
+    )
+    cart = Cart.new(
+      user: user,
+    ).save
+    expect(cart).to eq(true)
+  end
+  it 'cannot be without user' do
+    
+    cart = Cart.new(
+    ).save
+    expect(cart).to eq(false)
+  end
 end
